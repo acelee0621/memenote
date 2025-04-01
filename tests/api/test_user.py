cb61882a8b32d5f02824eb99d6b808dbbd3d7e2a
acelee0621 <acelee0621@outlook.com>
2025-04-01 11:23:50 +0800

    test

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -5,10 +5,10 @@ from app.schemas.schemas import UserResponse
 
 @pytest.mark.filterwarnings("ignore:Accessing argon2.__version__:DeprecationWarning")
 @pytest.mark.asyncio
-async def test_users_me(auth_client:AsyncClient, test_user:UserResponse):    
-    response = await auth_client.get("/users/me")
+async def test_users_me(authorized_client: AsyncClient, test_user: UserResponse):
+    response = await authorized_client.get("/users/me")
     assert response.status_code == 200
     user_response = UserResponse.model_validate(response.json())
-
+    
     assert user_response == test_user
     assert "password_hash" not in response.json()
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,44 +9,37 @@ from sqlalchemy.pool import NullPool
 from app.main import app
 from app.models.models import Base
 from app.core.database import get_db
-from app.core.security import get_current_user
-from app.models.models import User
-from app.schemas.schemas import UserResponse
+from app.core.security import create_access_token, get_current_user
+from app.repository.user_repo import UserRepository
+from app.schemas.schemas import UserCreate, UserResponse
+from .helper import the_first_user
 
 
 TEST_DATABASE_URL = (
     "postgresql+asyncpg://postgres:postgres@localhost:5432/memenote_test"
 )
 
-
 test_engine = create_async_engine(
     TEST_DATABASE_URL,
-    poolclass=NullPool,
+    # poolclass=NullPool,
     # echo=True
 )
-
 
 TestingSessionLocal = async_sessionmaker(
     class_=AsyncSession, expire_on_commit=False, bind=test_engine
 )
 
 
-@pytest_asyncio.fixture
-async def db_session() -> AsyncGenerator[AsyncSession, None]:
-    async with TestingSessionLocal() as session:
-        yield session
-
-
 # https://pypi.org/project/pytest-async-sqlalchemy/
 # or poolclass = NullPool when create_async_engine
-# @pytest_asyncio.fixture(scope="session", autouse=True)
-# def event_loop():
-#     """
-#     Creates an instance of the default event loop for the test session.
-#     """
-#     loop = asyncio.new_event_loop()
-#     yield loop
-#     loop.close()
+@pytest_asyncio.fixture(scope="session", autouse=True)
+def event_loop():
+    """
+    Creates an instance of the default event loop for the test session.
+    """
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest_asyncio.fixture(scope="session", autouse=True)
@@ -58,56 +51,49 @@ async def setup_db():
         await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest_asyncio.fixture
-async def override_get_db(db_session: AsyncSession):
-    async def _override_get_db():
-        yield db_session
-    return _override_get_db
+@pytest_asyncio.fixture(scope="session")
+async def override_get_db():
+    async with TestingSessionLocal() as session:
+        yield session
 
 
 # 模拟一个用于测试认证的用户
-@pytest_asyncio.fixture
-async def test_user(db_session: AsyncSession):
-    from app.core.security import get_password_hash
-    from .helper import the_first_user
-
-    the_first_user["password_hash"] = get_password_hash(the_first_user.pop("password"))
-
-    user = User(**the_first_user)
-    db_session.add(user)
-    await db_session.commit()
-    await db_session.refresh(user)
-    return UserResponse.model_validate(user)
-    # return user
+@pytest_asyncio.fixture(scope="session")
+async def test_user(override_get_db: AsyncSession):
+    test_user = await UserRepository(override_get_db).create(UserCreate(**the_first_user))
+    return UserResponse.model_validate(test_user)
 
 
 # 测试客户端 fixture，包含依赖覆盖和清理
-@pytest_asyncio.fixture
-async def client(override_get_db) -> AsyncGenerator[AsyncClient, None]:
+@pytest_asyncio.fixture(scope="session")
+async def client() -> AsyncGenerator[AsyncClient, None]:
     # 设置依赖项覆盖
     app.dependency_overrides[get_db] = override_get_db
 
     async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+        headers={"Content-Type": "application/json"},
     ) as ac:
         yield ac
     # 清理依赖项覆盖
     app.dependency_overrides.clear()
 
 
-@pytest_asyncio.fixture
-async def auth_client(
-    client: AsyncClient, test_user: UserResponse
+@pytest_asyncio.fixture(scope="session")
+async def token(test_user: UserResponse) -> str:
+    token = create_access_token({"sub": test_user.username})
+    return token
+
+
+@pytest_asyncio.fixture(scope="session")
+async def authorized_client(
+    client: AsyncClient, token: str, test_user: UserResponse
 ) -> AsyncGenerator[AsyncClient, None]:
+    client.headers = {
+        "Authorization": f"Bearer {token}",
+        **client.headers,
+    }
     app.dependency_overrides[get_current_user] = lambda: test_user
     yield client
-    app.dependency_overrides.pop(get_current_user, None)
-
-
-# 不带认证的默认 client
-@pytest_asyncio.fixture
-async def non_auth_client():
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        yield ac
+    app.dependency_overrides.clear()
